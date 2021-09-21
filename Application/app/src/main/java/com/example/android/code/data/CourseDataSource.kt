6bf81a7677714c85e7843a00c866aed6836d6641
Devrath <devrath.dev595@gmail.com>
2021-09-21 11:44:54 +0530

    ss

--- Application/app/src/main/java/com/example/android/code/data/CourseDataSource.kt
+++ Application/app/src/main/java/com/example/android/code/data/CourseDataSource.kt
@@ -1,46 +1,33 @@
 package com.example.android.code.data
 
 import com.example.android.code.data.model.Course
-import com.example.android.code.data.model.CourseLevel
 import kotlinx.coroutines.flow.flowOf
 
 fun getCourseList() = flowOf(
   listOf(
     Course(
       name = "Apache Kafka Series",
-      description = "Kafka - Master Avro, the Confluent Schema Registry and Kafka REST Proxy. Build Avro Producers/Consumers, Evolve Schemas",
-      level = CourseLevel.BEGINNER,
-      completed = true
+      description = "Kafka - Master Avro, the Confluent Schema Registry and Kafka REST Proxy. Build Avro Producers/Consumers, Evolve Schemas"
     ),
     Course(
       name = "WireMock for Java Developers",
-      description = "Learn to build robust RESTFUL API Clients, Service Virtualization in Java and SpringBoot applications using WireMock.",
-      level = CourseLevel.BEGINNER,
-      completed = false
+      description = "Learn to build robust RESTFUL API Clients, Service Virtualization in Java and SpringBoot applications using WireMock."
     ),
     Course(
       name = "Develop Minecraft Plugins",
-      description = "High-quality tutorials providing you with the most sustainable practices available to make Minecraft plugins.",
-      level = CourseLevel.ADVANCED,
-      completed = false
+      description = "High-quality tutorials providing you with the most sustainable practices available to make Minecraft plugins."
     ),
     Course(
       name = "Introduction to Apache Solr 8",
-      description = "Fundamental concepts and use-cases of Apache Solr",
-      level = CourseLevel.BEGINNER,
-      completed = false
+      description = "Fundamental concepts and use-cases of Apache Solr"
     ),
     Course(
       name = "Unit Testing Swift Mobile App",
-      description = "Apply Test-Driven Development(TDD) process to iOS mobile app development in Swift",
-      level = CourseLevel.ADVANCED,
-      completed = true
+      description = "Apply Test-Driven Development(TDD) process to iOS mobile app development in Swift"
     ),
     Course(
       name = "Ionic - Build with Ionic & Angular",
-      description = "Build Native iOS & Android as well as Progressive Web Apps with Angular, Capacitor and the Ionic Framework (Ionic 4+).",
-      level = CourseLevel.BEGINNER,
-      completed = false
+      description = "Build Native iOS & Android as well as Progressive Web Apps with Angular, Capacitor and the Ionic Framework (Ionic 4+)."
     )
   )
 )
--- Application/app/src/main/java/com/example/android/code/data/model/Course.kt
+++ Application/app/src/main/java/com/example/android/code/data/model/Course.kt
@@ -2,9 +2,5 @@ package com.example.android.code.data.model
 
 data class Course(
   val name: String,
-  val description: String,
-  val level: CourseLevel,
-  val completed: Boolean
+  val description: String
 )
-
-enum class CourseLevel { BEGINNER, ADVANCED }
